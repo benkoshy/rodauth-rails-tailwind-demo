fa77c3c839ba2569dba41d81873d35c2b7258239
BK <benkoshy@hotmail.com>
2022-08-20 12:19:10 +1000

    Temp commit - must revert - temporarily switch off recovery codes

--- app/misc/rodauth_main.rb
+++ app/misc/rodauth_main.rb
@@ -4,8 +4,9 @@ class RodauthMain < Rodauth::Rails::Auth
     enable :create_account, :verify_account, :verify_account_grace_period,
       :login, :logout, :remember,
       :reset_password, :change_password, :change_password_notify,
-      :change_login, :verify_login_change, :close_account,  :recovery_codes,
+      :change_login, :verify_login_change, :close_account,  
       :email_auth, :lockout, :confirm_password, :otp
+      # :recovery_codes
 
     # See the Rodauth documentation for the list of available config options:
     # http://rodauth.jeremyevans.net/documentation.html
